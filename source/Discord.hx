ef786bd008116489fe6f47fd00763b65a6ca53f6
Joalor64 <rhgarcon@gmail.com>
2022-05-25 16:30:52 -0400

    discord rpc edit

--- source/Discord.hx
+++ source/Discord.hx
@@ -45,7 +45,7 @@ class DiscordClient
 			details: "In the Menus",
 			state: null,
 			largeImageKey: 'icon',
-			largeImageText: "BEAT! Engine"
+			largeImageText: "Joalor64 Engine PLUS"
 		});
 	}
 
